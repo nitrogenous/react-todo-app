8b58f2dd638dc714f7523e2a45b951ce09e0ccc1
toprakkoc <hello@toprak.dev>
2021-03-13 21:10:45 +0300

    first test cases

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const [items, setItems] = React.useState([]);
+  const [text, setText] = React.useState("");
+
+  const handleChange = (e) => setText(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!text.length) {
+      return;
+    }
+
+    const newItem = {
+      text,
+      id: Date.now()
+    };
+
+    setText("");
+    setItems(items.concat(newItem));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TODOS</h1>
+
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="new-todo">What needs to be done?</label>
+        <br />
+        <input id="new-todo" value={text} onChange={handleChange} />
+        <button>Add #{items.length + 1}</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders application', () => {
+  const { getByText, getByLabelText } = render(<App />);
+
+  getByText('TODOS');
+  getByLabelText('What needs to be done?');
+  getByText('Add #1');
+});
+
+test('allows users to add items', () => {
+  const { getByText, getByLabelText } = render(<App />);
+
+  const itemInput = getByLabelText('What needs to be done?');
+  const addButton = getByText(/Add/g);
+
+  fireEvent.change(itemInput, { target: { value: 'Learn how to write test' } });
+  fireEvent.click(addButton);
+
+  expect(getByText('Learn how to write test')).toBeInTheDocument();
+});
+
+test('wont allows users to add null items', () => {
+  const { getByText, container } = render(<App />);
+
+  const addButton = getByText(/Add/g);
+
+  fireEvent.click(addButton);
+
+  expect(container.querySelector('li')).not.toBeInTheDocument();
+});
+
+test('users can create 500 items', () => {
+  const { getByText, getByLabelText } = render(<App />);
+
+  const itemInput = getByLabelText('What needs to be done?');
+  const addButton = getByText(/Add/g);
+
+  for (let i = 0; i < 500; i++) {
+    fireEvent.change(itemInput, { target: { value: i } });
+    fireEvent.click(addButton);
+
+    getByText(i);
+  }
 });
